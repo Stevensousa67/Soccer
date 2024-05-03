7d1dc7f68fb5888f43aecf8430e05de1f0b4476d
Steven Sousa <silver.steven67@gmail.com>
2024-05-03 18:51:59 -0400

    added port 3000

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const { v4: uuid } = require('uuid');
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var app = express();
+const port = process.env.PORT || 3000;
 
 var session = require('express-session');
 var flash = require('express-flash');
