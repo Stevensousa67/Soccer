16557c1864ab19fb418e5b52207473207a32d832
Steven Sousa <silver.steven67@gmail.com>
2024-05-03 22:38:01 -0400

    hid scoreboardLink & myStatsLink

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -4,8 +4,8 @@ fetch('/checkAuth')
         if (data.isAuthenticated) {
             document.getElementById('authButtons').classList.add('d-none'); // Hide buttons for non-authenticated users
             document.getElementById('loggedInButtons').classList.remove('d-none'); // Show buttons for authenticated users
-            document.getElementById('scoreboardLink').classList.remove('d-none'); // Show Scoreboard link
-            document.getElementById('myStatsLink').classList.remove('d-none'); // Show My Team's Stats link
+            //document.getElementById('scoreboardLink').classList.remove('d-none'); // Show Scoreboard link
+            //document.getElementById('myStatsLink').classList.remove('d-none'); // Show My Team's Stats link
             if (data.logoUrl) {
                 const logo = document.getElementById('logo');
                 logo.src = data.logoUrl; // Update logo src with team logo URL
